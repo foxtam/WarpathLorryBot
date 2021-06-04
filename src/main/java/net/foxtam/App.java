f5f07296ff179ee869d85f365a270ced5a7e468a
Fox <foxtam7@ya.ru>
2021-06-04 11:37:37 +0300

    Add Warpath class

--- src/main/java/net/foxtam/App.java
+++ src/main/java/net/foxtam/App.java
@@ -1,13 +1,13 @@
 package net.foxtam;
 
+import net.foxtam.bot.WarpathBot;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new WarpathBot().run();
     }
 }
---
+++ src/main/java/net/foxtam/bot/WarpathBot.java
@@ -0,0 +1,15 @@
+package net.foxtam.bot;
+
+import net.foxtam.Bot;
+import net.foxtam.KeyConfig;
+
+public class WarpathBot extends Bot {
+    public WarpathBot() {
+        super("NoxPlayer", KeyConfig.getDefault());
+    }
+
+    @Override
+    protected void action() {
+        
+    }
+}
