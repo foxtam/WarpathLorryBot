cd68ba1da1b37e05710a85cf33cc5c012091ea30
Fox <foxtam7@ya.ru>
2021-06-09 21:01:05 +0300

    Finish Factory bypass

--- src/main/java/net/foxtam/warpathlorry/SwingApp.java
+++ src/main/java/net/foxtam/warpathlorry/SwingApp.java
@@ -1,11 +1,62 @@
 package net.foxtam.warpathlorry;
 
-import net.foxtam.warpathlorry.window.BotFrame;
+import net.foxtam.warpathlorry.bot.WarpathBot;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 
-public class SwingApp {
+public class SwingApp extends JFrame {
+
+    static {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private final String ID = IDCalculator.getCurrentMachineID();
+
     public static void main(String[] args) throws Exception {
-        SwingUtilities.invokeAndWait(BotFrame::new);
+        SwingUtilities.invokeAndWait(SwingApp::new);
+    }
+
+    public SwingApp() {
+        super("Warpath Bot");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(280, 240);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(0, 1, 10, 10));
+        panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        String message = String.format("<html>Pause bot: F4<br>Stop bot: F8<br><br>ID: %s</html>", ID);
+        JLabel label = new JLabel(message);
+        panel.add(label);
+
+        JButton copyIdButton = new JButton("Copy ID");
+        copyIdButton.addActionListener(
+            e -> {
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                StringSelection selection = new StringSelection(ID);
+                clipboard.setContents(selection, selection);
+            });
+        panel.add(copyIdButton);
+
+        JButton runButton = new JButton("Run");
+        runButton.addActionListener(e -> {
+            runButton.setEnabled(false);
+            new WarpathBot().run();
+            runButton.setEnabled(true);
+        });
+        panel.add(runButton);
+
+        add(panel);
+//        pack();
+        setVisible(true);
     }
 }
--- src/main/java/net/foxtam/warpathlorry/bot/WarpathBot.java
+++ src/main/java/net/foxtam/warpathlorry/bot/WarpathBot.java
@@ -3,6 +3,7 @@ package net.foxtam.warpathlorry.bot;
 
 import net.foxtam.foxclicker.*;
 
+import java.util.List;
 import java.util.Random;
 
 public class WarpathBot extends Bot {
@@ -12,14 +13,44 @@ public class WarpathBot extends Bot {
     final Image recallLorryButton = Image.loadFromResource("/images/recall_lorry_button.png");
     final Image lorry = Image.loadFromResource("/images/lorry.png");
     final Image sleepingLorryIcon = Image.loadFromResource("/images/sleeping_lorry_icon.png");
-    final Image farm = Image.loadFromResource("/images/farm.png");
+    final Image farm = Image.loadFromResource("/images/factory_building.png");
     final Image mine = Image.loadFromResource("/images/mine.png");
     final Image oilWell = Image.loadFromResource("/images/oil_well.png");
     final Image searchButton = Image.loadFromResource("/images/search_button.png");
     final Image roundSendLorryButton = Image.loadFromResource("/images/round_send_lorry_button.png");
+    final Image returnToBaseButton = Image.loadFromResource("/images/return_to_base_button.png");
+    final Image factoryBuilding = Image.loadFromResource("/images/factory_building.png");
+    final Image productionRoundButton = Image.loadFromResource("/images/production_round_button.png");
+    final Image inStock = Image.loadFromResource("/images/in_stock.png");
+    final Image productionGreenButton = Image.loadFromResource("/images/production_green_button.png");
+    final Image productLeftArrows = Image.loadFromResource("/images/product_left_arrows.png");
+    final Image productRightArrows = Image.loadFromResource("/images/product_right_arrows.png");
+    final Image redSlash = Image.loadFromResource("/images/red_slash.png");
+    final Image whiteSlash = Image.loadFromResource("/images/white_slash.png");
+    final Image backToMainScreenButton = Image.loadFromResource("/images/back_to_main_screen_button.png");
+
+    final List<Image[]> workshops =
+        List.of(
+            new Image[]{
+                Image.loadFromResource("/images/workshop_1_1.png"),
+                Image.loadFromResource("/images/workshop_1_2.png")
+            },
+            new Image[]{
+                Image.loadFromResource("/images/workshop_2_1.png"),
+                Image.loadFromResource("/images/workshop_2_2.png"),
+            },
+            new Image[]{
+                Image.loadFromResource("/images/workshop_3_1.png"),
+                Image.loadFromResource("/images/workshop_3_2.png")
+            },
+            new Image[]{
+                Image.loadFromResource("/images/workshop_4_1.png"),
+                Image.loadFromResource("/images/workshop_4_2.png")
+            });
+
 
     private final Random random = new Random();
-
+    private final Finder finder = new Finder(4.0, 0.90, false);
     private Direction lorryDragDirection = Direction.LEFT;
 
     public WarpathBot() {
@@ -29,34 +60,60 @@ public class WarpathBot extends Bot {
     @Override
     protected void action() {
         while (true) {
-            bypass();
-            sleep(5);
+            lorryBypass();
+            sleep(3);
+
+            factoryBypass();
+            sleep(3);
         }
     }
 
-    private void bypass() {
-        openBottomLorryWindow();
-        if (!canSeeSleepingLorry()) showSleepingLorry();
-        if (!canSeeSleepingLorry()) return;
-        sendSleepingLorry();
+    private void lorryBypass() {
+        while (true) {
+            openBottomLorryWindow();
+            if (!canSeeSleepingLorry()) showSleepingLorry();
+            if (!canSeeSleepingLorry()) {
+                finder.leftClickOn(lorryMainButton);
+                return;
+            }
+            sendSleepingLorry();
+        }
+    }
+
+    private void factoryBypass() {
+        finder.leftClickOn(returnToBaseButton);
+        do {
+            finder.leftClickOn(factoryBuilding);
+        } while (!finder.isImageVisible(productionRoundButton));
+        finder.leftClickOn(productionRoundButton);
+        finder.waitForImage(productionGreenButton);
+
+        shiftProductsToRight();
+        for (Image[] workshop : workshops) {
+            tapNextWorkshop(workshop);
+            oneTapToRight();
+            orderProduct();
+        }
+
+        finder.leftClickOn(backToMainScreenButton);
     }
 
     private void openBottomLorryWindow() {
         for (int i = 0; i < 2; i++) {
-            if (!isImageVisible(lorry)) {
-                leftClickOn(lorryMainButton);
-                sleep(1);
+            if (!finder.isImageVisible(lorry)) {
+                finder.leftClickOn(lorryMainButton);
+                sleep(1); 
             }
         }
-        waitForImage(lorry, 3);
+        finder.waitForImage(lorry);
     }
 
     private boolean canSeeSleepingLorry() {
-        return isImageVisible(sleepingLorryIcon);
+        return finder.isImageVisible(sleepingLorryIcon);
     }
 
     private void showSleepingLorry() {
-        mouseMoveTo(lorry);
+        finder.mouseMoveTo(lorry);
         mouseDragDirection(lorryDragDirection, 300);
         lorryDragDirection =
             lorryDragDirection == Direction.LEFT
@@ -65,15 +122,35 @@ public class WarpathBot extends Bot {
     }
 
     private void sendSleepingLorry() {
-        leftClickOn(sleepingLorryIcon);
-        waitForImage(sendLorryButton, 5);
-        leftClickOn(sendLorryButton);
+        finder.leftClickOn(sleepingLorryIcon);
+        finder.leftClickOn(sendLorryButton);
         chooseDestinationType();
-        leftClickOn(searchButton);
+        finder.leftClickOn(searchButton);
         sleep(2);
         leftClickAt(getWindowCenterPoint());
-        waitForImage(roundSendLorryButton, 5);
-        leftClickOn(roundSendLorryButton);
+        finder.leftClickOn(roundSendLorryButton);
+    }
+
+    private void shiftProductsToRight() {
+        while (finder.isImageVisible(productLeftArrows)) {
+            leftClickAt(finder.getCenterPointOf(productLeftArrows).shift(120, 70));
+            sleep(0.2);
+        }
+    }
+
+    private void tapNextWorkshop(Image[] workshopImages) {
+        finder.leftClickAnyImage(workshopImages);
+    }
+
+    private void oneTapToRight() {
+        leftClickAt(finder.getCenterPointOf(productRightArrows).shift(-120, 70));
+        sleep(0.5);
+    }
+
+    private void orderProduct() {
+        while (finder.withInColor(true).isImageVisible(whiteSlash)) {
+            finder.leftClickOn(productionGreenButton);
+        }
     }
 
     private void chooseDestinationType() {
@@ -81,7 +158,7 @@ public class WarpathBot extends Bot {
         Image choice;
         do {
             choice = destinations[random.nextInt(destinations.length)];
-        } while (!isImageVisible(choice));
-        leftClickOn(choice);
+        } while (!finder.isImageVisible(choice));
+        finder.leftClickOn(choice);
     }
 }
