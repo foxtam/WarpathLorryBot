996e801ad35df01fce863c4c09b398e3520a0a4a
Fox <foxtam7@ya.ru>
2021-06-10 13:55:36 +0300

    Add bot properties

---
+++ src/main/java/net/foxtam/warpathlorry/BotProperties.java
@@ -0,0 +1,29 @@
+package net.foxtam.warpathlorry;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+public class BotProperties {
+
+    private static final Path propertiesPath = Path.of("config.properties");
+
+    public static String getServerIP() {
+        return getProperty("server_ip");
+    }
+
+    private static String getProperty(String key) {
+        try {
+            Properties properties = new Properties();
+            properties.load(Files.newBufferedReader(propertiesPath));
+            return properties.getProperty(key);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int getServerPort() {
+        return Integer.parseInt(getProperty("server_port"));
+    }
+}
