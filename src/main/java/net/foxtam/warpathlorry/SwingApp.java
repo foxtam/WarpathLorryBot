7148329b3ee9e92f465d03b18f7b8873c138ae4a
Fox <foxtam7@ya.ru>
2021-06-10 13:56:49 +0300

    Add server communication

--- src/main/java/net/foxtam/warpathlorry/SwingApp.java
+++ src/main/java/net/foxtam/warpathlorry/SwingApp.java
@@ -7,18 +7,24 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class SwingApp extends JFrame {
 
     static {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     private final String ID = IDCalculator.getCurrentMachineID();
+    private final String serverIP = BotProperties.getServerIP();
+    private final int serverPort = BotProperties.getServerPort();
 
     public static void main(String[] args) throws Exception {
         SwingUtilities.invokeAndWait(SwingApp::new);
@@ -53,10 +59,46 @@ public class SwingApp extends JFrame {
             new WarpathBot().run();
             runButton.setEnabled(true);
         });
+        runButton.setEnabled(false);
         panel.add(runButton);
 
         add(panel);
-//        pack();
+
+        SwingUtilities.invokeLater(communicateToServer(runButton));
+
         setVisible(true);
+    }
+
+    private Runnable communicateToServer(JButton runButton) {
+        return () -> {
+            try (
+                Socket socket = new Socket(serverIP, serverPort);
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())
+            ) {
+                dataOutputStream.writeUTF(ID);
+                boolean serverResponse = dataInputStream.readBoolean();
+                String id = dataInputStream.readUTF();
+                JOptionPane.showMessageDialog(this, serverResponse + " for " + id);
+                runButton.setEnabled(serverResponse);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private void talkToServer() {
+        try (
+            Socket socket = new Socket("127.0.0.1", 41379);
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())
+        ) {
+            dataOutputStream.writeUTF("hello");
+            dataInputStream.readBoolean();
+            String input = dataInputStream.readUTF();
+            JOptionPane.showMessageDialog(this, input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
