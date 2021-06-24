a5a3ae625d01d4d8dae37ad10acc5f708961eccf
Fox <foxtam7@ya.ru>
2021-06-24 12:11:14 +0300

    Update class Licenses

--- src/main/java/net/foxtam/warpathlorry/Licenses.java
+++ src/main/java/net/foxtam/warpathlorry/Licenses.java
@@ -3,43 +3,57 @@ package net.foxtam.warpathlorry;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class Licenses {
 
-    private static final String clientsFile = "https://garantmarket.net/warpath/warpath_clients.json";
-    private static final boolean LOCAL_TEST = false;
+    private static final URL clientsFileFromServer = toURL("https://garantmarket.net/warpath/warpath_clients.json");
+    private static final URL localClientsFile = toURL(Path.of("warpath_clients.json"));
 
-    public static Optional<LocalDate> getExpirationDateOf(ComputerID id) throws IOException {
-        return tryGetClientExpirationDate(id);
+    private static final java.lang.reflect.Type type =
+            new TypeToken<Map<String, String>>() {
+            }.getType();
+
+    private static URL toURL(Path path) {
+        try {
+            return path.toUri().toURL();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static Optional<LocalDate> tryGetClientExpirationDate(ComputerID id) throws IOException {
-        Gson gson = new Gson();
+    public static Optional<LocalDate> getExpirationDateOf(ComputerID id) {
+        var gson = new Gson();
 
-        java.lang.reflect.Type type =
-              new TypeToken<Map<String, String>>() {
-              }.getType();
-
-        var jsonReader = new InputStreamReader(getClientsFileInputStream(), StandardCharsets.UTF_8);
-        Map<String, String> map = gson.fromJson(jsonReader, type);
+        String jsonString = readStringFromURL(localClientsFile);
+        Map<String, String> map = gson.fromJson(jsonString, type);
 
         String key = id.toString();
         return Optional.ofNullable(map.get(key)).map(LocalDate::parse);
     }
 
-    private static InputStream getClientsFileInputStream() throws IOException {
-        if (LOCAL_TEST) {
-            String clientsFile = "warpath_clients.json";
-            return new BufferedInputStream(new FileInputStream(clientsFile));
-        } else {
-            URL url = new URL(clientsFile);
-            return new BufferedInputStream(url.openStream());
+    public static String readStringFromURL(URL url) {
+        try (Scanner scanner = new Scanner(url.openStream(), StandardCharsets.UTF_8)) {
+            scanner.useDelimiter("\\A");
+            return scanner.hasNext() ? scanner.next() : "";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static URL toURL(String string) {
+        try {
+            return new URL(string);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
